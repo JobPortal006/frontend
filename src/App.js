6179714135e7842f6d115190fd1a7f18b54c09c8
Raghultom <raghultom27@gmail.com>
2024-01-05 17:29:52 +0530

    hlo tom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Navbar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,60 @@
-import React from 'react'
+// Navbar.jsx
+import React from 'react';
+import '../components/Navbar.css';
+import { FaUserCircle, FaBell } from 'react-icons/fa';
+
+
 
 const Navbar = () => {
-  return (
-    <div>
-      hlo
-    </div>
-  )
-}
+    return (
+        <div className="Navbar">
+            <div className="Navbar__left">
+                <img src="your-logo.png" alt="Logo" className="Navbar__logo" />
+            </div>
+            <div className="Navbar__center">
+                <ul>
+                    <li className="Navbar__dropdown">
+                        Home
+                        <div className="Navbar__dropdown-content">
+                            {/* Dropdown content for Home */}
+                            {/* Add your links or components here */}
+                        </div>
+                    </li>
+                    <li className="Navbar__dropdown">
+                        Employer
+                        <div className="Navbar__dropdown-content">
+                            {/* Dropdown content for Employer Jobs */}
+                            <ul>
+                                <li>Employer details	</li>
+                                <li>Add job posting	</li>
+                                {/* Add more names or links as needed */}
+                            </ul>
+                        </div>
+                    </li>
+                    <li className="Navbar__dropdown">
+                        Candidate Jobs
+                        <div className="Navbar__dropdown-content">
+                            <ul>
+                                <li>Browse job</li>
+                                <li>Job categories</li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            
 
-export default Navbar
+            <div className="Navbar__right">
+                <button className="Navbar__button">Login</button>
+                <button className="Navbar__button">Recruiters Login</button>
+                <div className="Navbar__profile">
+                    <FaBell className="Navbar__notification-icon" />
+                    <FaUserCircle className="Navbar__user-icon"  style={{fontSize:'20px'}}/>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
