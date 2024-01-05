196ccc0a091d2f74774b526b37bb2791e00218e1
Raghultom <raghultom27@gmail.com>
2024-01-05 13:21:06 +0530

    hlo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-</div>
-)
-};
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
 
+function App() {
+  return (
+    <div className="App">
+     <Navbar />
+    </div>
+  );
+}
 
 export default App;
