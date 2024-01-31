67921392711bf38d10aa9be42ddf8c48d09b9d50
Jeeva <jeevaelango94@gmail.com>
2024-01-31 11:36:48 +0530

    Jeeva

--- create-playwright/tests/login.test.js
+++ create-playwright/tests/login.test.js
@@ -2,4 +2,17 @@ const { test, expect } = require('@playwright/test');
 
 test('Login', async ({page})=>{
     await page.goto('http://localhost:3000/login');
-})
+
+    const  emailField = page.locator('#email');
+    const passwordField = page.locator('#password');
+    const submitButton = page.locator("button[type='submit']");
+    
+    // Fill the form with some data
+    await emailField.fill('jeeva2002@gmail.com');
+    await passwordField.fill('Jeeva@123');
+
+    // Submit the form by clicking a button
+    await submitButton.click();
+});
+
+
