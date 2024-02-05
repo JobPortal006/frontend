465e756711c1c1f0c9da8f1289c362e247f721f1
RagulKtr <ragul.1803059@gmail.com>
2024-02-05 13:27:41 +0530

    r m

--- src/components/UserManagement/Signup.jsx
+++ src/components/UserManagement/Signup.jsx
@@ -33,7 +33,7 @@ import {
   handleSubmit,
   handleGoogleSignIn
 } from '../validation/signupvalidation';
-import Jobposting from '../../Jobposting';
+import Jobposting from './Jobposting';
 
 export default function FixedContainer() {
 
