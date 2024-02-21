eed18da9ce4ad846059561c106208c946db7fc6f
Raghultom <raghultom27@gmail.com>
2024-02-21 13:30:46 +0530

    image view

--- src/components/JobPostSample/JobPostSample.js
+++ src/components/JobPostSample/JobPostSample.js
@@ -1,7 +1,3 @@
-
-
-
-
 // import React, { useState, useEffect } from "react";
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faMapMarkerAlt, faMoneyBillAlt, faUser, faBuilding, faTools } from '@fortawesome/free-solid-svg-icons';
@@ -14,31 +10,42 @@
 //   const navigate = useNavigate();
 
 //     const [jobs, setJobs] = useState([]);
+//     console.log(jobs,"jobs")
+//     const [loading, setLoading] = useState(false); // Track loading state
 
+//     //pradhap======>
 //     useEffect(() => { 
 //         async function fetchJobs() {
 //             try {
-//                 const response = await fetch('http://192.168.1.38:8000/get_view_jobs/');
+//                 setLoading(true); // Set loading state when fetching jobs starts
+//                 const response = await fetch('http://192.168.1.39:8000/get_view_jobs/');
 //                 if (!response.ok) {
 //                     throw new Error('Failed to fetch jobs');
 //                 }
-//                 const data = await response.json();
-//                 console.log(data);
-//                 if (data && Array.isArray(data.data) && Array.isArray(data.data[0])) {
-//                     setJobs(data.data[0]);
+//                 const responseData = await response.json();
+//                 console.log(responseData);
+//                 const { status, statusCode, message, data } = responseData;
+//                 if (status && statusCode === 200 && Array.isArray(data) && data.length > 0) {
+//                     setJobs(data); // Update jobs state with the fetched data
 //                 } else {
-//                     console.error('Invalid data format received from API');
+//                     console.error('Invalid data format received from API:', message);
 //                 }
 //             } catch (error) {
 //                 console.error('Error fetching jobs:', error);
+//             } finally {
+//                 setLoading(false); // Reset loading state when fetching jobs completes
 //             }
-//         }
+//         }   
 //         fetchJobs();
 //     }, []);
 
+
+
+
 //     const handleJobSelect = async (selectedJob) => {
 //         try {
-//             const response = await fetch('http://192.168.1.38:8000/job_details/', {
+//             setLoading(true); // Set loading state when fetching job details starts
+//             const response = await fetch('http://192.168.1.39:8000/job_details/', {
 //                 method: 'POST',
 //                 headers: {
 //                     'Content-Type': 'application/json',
@@ -47,59 +54,70 @@
 //             });
 //             if (!response.ok) {
 //                 throw new Error('Failed to send selected job data to the backend');
-//             }else{
+//             } else {
 //                 navigate('/JobDetails');
-
 //             }
 //             console.log('Selected job data sent successfully:', selectedJob);
 //             // Add any further handling as needed
 //         } catch (error) {
 //             console.error('Error sending selected job data to the backend:', error);
+//         } finally {
+//             setLoading(false); // Reset loading state when fetching job details completes
 //         }
 //     };
 
 //     return (
 //         <>
-//             <div className="job-container">
-//                 <SearchBar isJobSearchPage={true} />
-//                 {jobs.map((job, index) => (
-//                     <div key={index} className="job-card" onClick={() => handleJobSelect(job)}>
-//                         <div className="job-header">
-//                             <div className="job-title">{job.job_title}</div>
-//                             <div className="company-name">{job.company_name}</div>
+//             {loading ? (
+//                 <div className="loading-popup">Loading...</div> // Render loading popup
+//             ) : (
+//                 <div className="job-container" style={{marginTop:'60px'}}>
+//                     <SearchBar isJobSearchPage={true} />
+//                     {jobs.map((job, index) => (
+//                         <div key={index} className="job-card" onClick={() => handleJobSelect(job)}>
+//                             <div className="job-header">
+//                                 <div className="job-title">{job.job_title}</div>
+//                                 <div className="company-name">{job.company_name}</div>
+//                             </div>
+//                             <div className="job-details">
+//                                 <div className="detail">
+//                                     <span className="detail-label"><FontAwesomeIcon icon={faMapMarkerAlt} /> Location:</span> {job.location}
+//                                 </div>
+//                                 <div className="detail">
+//                                     <span className="detail-label"><FontAwesomeIcon icon={faMoneyBillAlt} /> Salary:</span> {job.salary_range}
+//                                 </div>
+//                                 <div className="detail">
+//                                     <span className="detail-label"><FontAwesomeIcon icon={faUser} /> Job Type:</span> {job.employee_type}
+//                                 </div>
+//                                 <div className="detail">
+//                                     <span className="detail-label"><FontAwesomeIcon icon={faTools} /> Qualification:</span> {job.job_role}
+//                                 </div>
+//                                 <div className="detail">
+//                                     <span className="detail-label"><FontAwesomeIcon icon={faUser} /> Experience:</span> {job.experience}
+//                                 </div>
+//                                 <div className="detail">
+//                                     <span className="detail-label"><FontAwesomeIcon icon={faBuilding} /> Vacancies:</span> {job.no_of_vacancies}
+//                                 </div>
+//                                 <div className="detail">
+//                                     <span className="detail-label"><FontAwesomeIcon icon={faTools} /> Skills:</span> {job.skills ? job.skills.join(', ') : ''}
+//                                 </div>
+//                             </div>
 //                         </div>
-//                         <div className="job-details">
-//                             <div className="detail">
-//                                 <span className="detail-label"><FontAwesomeIcon icon={faMapMarkerAlt} /> Location:</span> {job.location}
-//                             </div>
-//                             <div className="detail">
-//                                 <span className="detail-label"><FontAwesomeIcon icon={faMoneyBillAlt} /> Salary:</span> {job.salary_range}
-//                             </div>
-//                             <div className="detail">
-//                                 <span className="detail-label"><FontAwesomeIcon icon={faUser} /> Job Type:</span> {job.employment_type}
-//                             </div>
-//                             <div className="detail">
-//                                 <span className="detail-label"><FontAwesomeIcon icon={faTools} /> Qualification:</span> {job.job_role}
-//                             </div>
-//                             <div className="detail">
-//                                 <span className="detail-label"><FontAwesomeIcon icon={faUser} /> Experience:</span> {job.experience}
-//                             </div>
-//                             <div className="detail">
-//                                 <span className="detail-label"><FontAwesomeIcon icon={faBuilding} /> Vacancies:</span> {job.no_of_vacancies}
-//                             </div>
-//                             <div className="detail">
-//                                 <span className="detail-label"><FontAwesomeIcon icon={faTools} /> Skills:</span> {job.skills ? job.skills.join(', ') : ''}
-//                             </div>
-//                         </div>
-//                     </div>
-//                 ))}
-//             </div>
+//                     ))}
+//                 </div>
+//             )}
 //         </> 
 //     );
 // }
 
 // export default JobPostSample;
 
+
+
+
+
+
+//checking====================================================>
 
 
 import React, { useState, useEffect } from "react";
@@ -109,42 +127,13 @@ import './JobPostSampleStyle.css';
 import SearchBar from "../HomePage/searchBar";
 import { useNavigate } from 'react-router-dom';
 
-
 function JobPostSample(props) {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
     const [jobs, setJobs] = useState([]);
-    console.log(jobs,"jobs")
     const [loading, setLoading] = useState(false); // Track loading state
 
-    // useEffect(() => { 
-    //     async function fetchJobs() {
-    //         try {
-    //             setLoading(true); // Set loading state when fetching jobs starts
-    //             const response = await fetch('http://192.168.1.38:8000/get_view_jobs/');
-    //             if (!response.ok) {
-    //                 throw new Error('Failed to fetch jobs');
-    //             }
-    //             const data = await response.json();
-    //             console.log(data);
-    //             console.log(data.data[0],'1-------------');
-    //             if (data && Array.isArray(data?.data) && Array.isArray(data?.data[0])) {
-    //                 setJobs(data.data[0]);
-    //             } else {
-    //                 console.error('Invalid data format received from API ===>pradhap api ');
-    //             }
-    //         } catch (error) {
-    //             console.error('Error fetching jobs:', error);
-    //         } finally {
-    //             setLoading(false); // Reset loading state when fetching jobs completes
-    //         }
-    //     }   
-    //     fetchJobs();
-    // }, []);
-
-
-    //pradhap======>
-    useEffect(() => { 
+    useEffect(() => {
         async function fetchJobs() {
             try {
                 setLoading(true); // Set loading state when fetching jobs starts
@@ -165,12 +154,9 @@ function JobPostSample(props) {
             } finally {
                 setLoading(false); // Reset loading state when fetching jobs completes
             }
-        }   
+        }
         fetchJobs();
     }, []);
-    
-
-    
 
     const handleJobSelect = async (selectedJob) => {
         try {
@@ -201,11 +187,20 @@ function JobPostSample(props) {
             {loading ? (
                 <div className="loading-popup">Loading...</div> // Render loading popup
             ) : (
-                <div className="job-container" style={{marginTop:'60px'}}>
+                <div className="job-container" style={{ marginTop: '60px' }}>
                     <SearchBar isJobSearchPage={true} />
                     {jobs.map((job, index) => (
                         <div key={index} className="job-card" onClick={() => handleJobSelect(job)}>
                             <div className="job-header">
+                                <div className="company-logo">
+                                    {/* {console.log("Company Logo Data:", job.company_logo)} */}
+                                    {/* Determine the image format based on the company_logo data */}
+                                    {job.company_logo && job.company_logo.includes('data:image') ? (
+                                        <img src={job.company_logo} alt="Company Logo" />
+                                    ) : (
+                                        <img src={`data:image/jpeg;base64,${job.company_logo}`} alt="Company Logo" />
+                                    )}
+                                </div>
                                 <div className="job-title">{job.job_title}</div>
                                 <div className="company-name">{job.company_name}</div>
                             </div>
@@ -236,7 +231,7 @@ function JobPostSample(props) {
                     ))}
                 </div>
             )}
-        </> 
+        </>
     );
 }
 
