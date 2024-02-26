8f2e678648161043c24375e6279b61cfbe2f3118
JeevaElango <jeevaelango886@gmail.com>
2024-02-26 13:46:39 +0530

    mm

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import Filter from "./components/sprint 2/Filter.jsx";
 import MyJob from "./components/sprint 2/MyJob.jsx";
 import EditMyJob from "./components/sprint 2/EditMyJob.jsx";
 import FilteredResults from "./components/sprint 2/FilteredResults.jsx";
-import JobDetails from "./components/sprint 2/jobdiscriptions.js";
+import JobDetails from "./components/Sprint 2/jobdiscriptions.js";
 import UserContext from "./components/sprint 2/contextFilter.jsx";
 
 function App() {
